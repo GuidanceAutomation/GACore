d08a7f160b3e294d548eb45b9698702161930848
Martin Davies <martin.davies@guidanceautomation.com>
2019-08-13 14:29:53 +0100

    #4 adds semverdata
    Closes #4

---
+++ GACore.Test/TSemVerData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GACore.Test
+{
+    [TestFixture]
+    public class TSemVerData
+    {
+        [TestCase(1, 2, 3, 1, 2, 3, 0)]
+        [TestCase(2, 2, 3, 1, 2, 3, 1)]
+        [TestCase(1, 2, 4, 1, 2, 3, 1)]
+        [TestCase(1, 3, 3, 1, 2, 3, 1)]
+        [TestCase(1, 2, 3, 2, 2, 3, -1)]
+        [TestCase(1, 2, 3, 1, 3, 3, -1)]
+        [TestCase(1, 2, 3, 1, 2, 4, -1)]
+        public void CompareTo(int majorA, int minorA, int patchA, int majorB, int minorB, int patchB, int expected)
+        {
+            SemVerData semVerDataA = new SemVerData(majorA, minorA, patchA);
+
+            SemVerData semVerDataB = new SemVerData(majorB, minorB, patchB);
+
+            Assert.AreEqual(expected, semVerDataA.CompareTo(semVerDataB));
+        }
+    }
+}
---
+++ GACore/SemVerData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GACore
+{
+    [DataContract]
+    public class SemVerData : IComparable
+    {
+        [DataMember]
+        public int Major { get; set; }
+
+        [DataMember]
+        public int Minor { get; set; }
+
+        [DataMember]
+        public int Patch { get; set; }
+
+        public SemVerData(int major, int minor, int patch)
+        {
+            this.Major = major;
+            this.Minor = minor;
+            this.Patch = patch;
+        }
+
+        public string ToSummaryString => string.Format("{0}.{1}.{2}", Major, Minor, Patch);
+
+        public override string ToString() => ToSummaryString;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            SemVerData other = obj as SemVerData;
+
+            if (other == null) throw new ArgumentOutOfRangeException("object is not SemVerData");
+
+            int majorResult = Major.CompareTo(other.Major);
+
+            if (majorResult != 0) return majorResult;
+
+            int minorResult = Minor.CompareTo(other.Minor);
+
+            if (minorResult != 0) return minorResult;
+
+            return Patch.CompareTo(other.Patch);
+        }
+    }
+}
