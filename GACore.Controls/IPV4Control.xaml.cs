fd2ea48adeeb1eb4bb91a5a1b2ddcbd7ac768467
Martin Davies <martin.davies@guidanceautomation.com>
2019-09-09 15:01:33 +0100

    Simple text box
    Simple text box with regex for IP v4

---
+++ GACore.Controls/IPV4Control.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GACore.Controls
+{
+	public partial class IPV4Control : UserControl
+	{
+		private Regex regex = new Regex(@"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$");
+
+		public IPV4Control()
+		{
+			InitializeComponent();
+		}
+
+		public IPAddress ToIPV4Address()
+		{
+			Match match = regex.Match(ipV4TextBox.Text);
+
+			if (match.Success) return IPAddress.Parse(match.Value);
+			return null;
+		}
+
+		private void IpV4TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Match match = regex.Match(ipV4TextBox.Text);
+
+			if (!match.Success) ipV4TextBox.Background = Brushes.Crimson;
+			else ipV4TextBox.Background = Brushes.White;
+		}
+	}
+}
