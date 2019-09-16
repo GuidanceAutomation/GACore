84a5aa363544c88e25685d4e2473c6f05841141a
Martin Davies <martin.davies@guidanceautomation.com>
2019-09-16 11:59:02 +0100

    Adds a generic mailbox
    Adds generic mailbox and associated unit test(s).

---
+++ GACore.Test/FooMailbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace GACore.Test
+{
+	/// <summary>
+	/// Dev class for testing generic mailbox functionality. 
+	/// </summary>
+	internal class FooMailbox : GenericMailbox<int,IPAddress>
+	{
+		public FooMailbox(int id, IPAddress current)
+			:base(id, current)
+		{
+		}
+	}
+}
---
+++ GACore.Test/TGenericMailbox.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace GACore.Test
+{
+	[TestFixture]
+	public class TGenericMailbox
+	{
+		[Test]
+		public void InitNull()
+		{
+			Assert.Throws<ArgumentNullException>(() => new FooMailbox(1, null));
+		}
+
+		[Test]
+		public void Equals()
+		{
+			HashSet<FooMailbox> mailboxes = new HashSet<FooMailbox>();
+
+			FooMailbox fooMailbox = new FooMailbox(1, IPAddress.Loopback);
+
+			Assert.IsTrue(mailboxes.Add(fooMailbox));
+			Assert.IsFalse(mailboxes.Add(fooMailbox));
+
+			IPAddress oneNineTwo = IPAddress.Parse("192.168.0.1");
+
+			fooMailbox.Update(oneNineTwo);
+			Assert.AreEqual(oneNineTwo, fooMailbox.Mail);
+
+			Assert.IsFalse(mailboxes.Add(fooMailbox));
+		}
+	}
+}
