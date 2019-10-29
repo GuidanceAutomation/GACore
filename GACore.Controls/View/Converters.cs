1070da720a603639e61f71864cd3626c256a687b
Martin Davies <martin.davies@guidanceautomation.com>
2019-10-29 09:21:35 +0000

    MVVM tuneup

---
+++ GACore.Controls/View/Converters.cs
@@ -0,0 +1,56 @@
+﻿using GACore.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GACore.Controls.View
+{
+	public class DynamicLimiterStatusToOverlayTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			DynamicLimiterStatus status = (DynamicLimiterStatus)value;
+			BrushCollection brushCollection = BrushDictionaries.DynamicLimiterStatusBrushCollectionDictionary.GetBrushCollection(status);
+
+			BrushCollectionProperty property = (BrushCollectionProperty)parameter;
+			return brushCollection.GetProperty(property);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+
+	public sealed class NavigationStatusToOverlayTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			NavigationStatus status = (NavigationStatus)value;
+			BrushCollection brushCollection = BrushDictionaries.NavigationStatusBackgroundBrushCollectionDictionary.GetBrushCollection(status);
+
+			BrushCollectionProperty property = (BrushCollectionProperty)parameter;
+			return brushCollection.GetProperty(property);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+
+	public class PositionControlStatusToOverlayTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			PositionControlStatus status = (PositionControlStatus)value;
+			BrushCollection brushCollection = BrushDictionaries.PositionControlStatusBackgroundBrushCollectionDictionary.GetBrushCollection(status);
+
+			BrushCollectionProperty property = (BrushCollectionProperty)parameter;
+			return brushCollection.GetProperty(property);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
---
+++ GACore.Controls/View/KingpinStateView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using GACore.Architecture;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GACore.Controls.View
+{
+	/// <summary>
+	/// Interaction logic for KingpinStateView.xaml
+	/// </summary>
+	public partial class KingpinStateView : UserControl
+	{
+		public KingpinStateView()
+		{
+			InitializeComponent();
+			CompositionTarget.Rendering += CompositionTarget_Rendering;
+		}
+
+		private void CompositionTarget_Rendering(object sender, EventArgs e)
+		{
+			if (DataContext is IRefresh)
+				((IRefresh)DataContext).Refresh();
+		}
+	}
+}
