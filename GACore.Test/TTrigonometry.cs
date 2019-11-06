ae06ae708b9b6d4ad18ed8329d7652578d15970d
Martin Davies <martin.davies@guidanceautomation.com>
2019-11-06 15:01:37 +0000

    Adds Messenger and Logging
    Adds messenger utilitly for signaling between view models, and a logger for view models to throw exceptions to,

--- GACore.Test/TTrigonometry.cs
+++ GACore.Test/TTrigonometry.cs
@@ -7,6 +7,8 @@ namespace GACore.Test
 	[Category("Trigonometry")]
 	public class TTrigonometry
 	{
+		private static double tol = 1e-3;
+
 		[Test]
 		[TestCase(0, 0)]
 		[TestCase(Math.PI, Math.PI)]
@@ -14,6 +16,16 @@ namespace GACore.Test
 		public void PiWrap(double value, double expected)
 		{
 			Assert.AreEqual(expected, Trigonometry.PiWrap(value));
+		}
+
+		[Test]
+		[TestCase(0,0,0)]
+		[TestCase(0, Math.PI/2, Math.PI / 2)]
+		[TestCase(0, -Math.PI / 2, -Math.PI / 2)]
+		public void MinAngleRad(double angleA, double angleB, double expected)
+		{
+			double actual = Trigonometry.MinAngleRad(angleA, angleB);
+			Assert.That(expected, Is.EqualTo(actual).Within(tol));
 		}
 	}
 }
--- GACore/AbstractViewModel.cs
+++ GACore/AbstractViewModel.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NLog;
+using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GACore
 {
@@ -24,6 +21,8 @@ namespace GACore
 				HandleModelUpdate();
 			}
 		}
+
+		public Logger Logger { get; } = NLog.NLogManager.Instance.GetFileTargetLogger("ViewModel");
 
 		protected virtual void HandleModelUpdate()
 		{
