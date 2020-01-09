bd54a44c7ea4cccc9799729cc7c7e08dffa4836c
Martin Davies <martin.davies@guidanceautomation.com>
2020-01-09 11:06:22 +0000

    Adds a ribbon button

--- GACore.Controls/CheckboxButtonControl.cs
+++ GACore.Controls/CheckboxButtonControl.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace GACore.Controls
@@ -19,6 +14,26 @@ namespace GACore.Controls
 			get { return (bool)GetValue(IsCheckedProperty); }
 			set { SetValue(IsCheckedProperty, value); }
 		}
+
+		public string CheckedText
+		{
+			get { return (string)GetValue(CheckedTextProperty); }
+			set { SetValue(CheckedTextProperty, value); }
+		}
+
+		public string UncheckedText
+		{
+			get { return (string)GetValue(UncheckedTextProperty); }
+			set { SetValue(UncheckedTextProperty, value); }
+		}
+
+		public static readonly DependencyProperty CheckedTextProperty =
+			DependencyProperty.Register("CheckedText", typeof(string),
+			typeof(CheckboxButtonControl), new PropertyMetadata("Checked"));
+
+		public static readonly DependencyProperty UncheckedTextProperty =
+			DependencyProperty.Register("UncheckedText", typeof(string),
+			typeof(CheckboxButtonControl), new PropertyMetadata("Unchecked"));
 
 		static CheckboxButtonControl()
 		{
