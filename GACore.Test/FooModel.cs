02210a5e723f3ced255a0f1912998a0ff4631c2e
Martin Davies <martin.davies@guidanceautomation.com>
2020-02-28 10:16:19 +0000

    Adds unit test

---
+++ GACore.Test/FooModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GACore.Test
+{
+	public class FooModel
+	{
+		public Guid InstanceId { get; } = Guid.NewGuid();
+
+		public FooModel()
+		{
+		}
+
+		public override string ToString() => string.Format("FooModel InstanceId:{0}", InstanceId);
+	}
+}
---
+++ GACore.Test/TViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+
+namespace GACore.Test
+{
+	[TestFixture]
+	public class TViewModel
+	{
+		private AutoResetEvent propertyChangedSet = new AutoResetEvent(false);
+
+		private TimeSpan timeout { get; } = TimeSpan.FromSeconds(5);
+
+		[Test]
+		public void PropertyChanged()
+		{
+			FooModel fooModel = new FooModel();
+			FooViewModel fooViewModel = new FooViewModel();
+			fooViewModel.PropertyChanged += FooViewModel_PropertyChanged;
+
+			Assert.IsNull(fooViewModel.Model);
+
+			fooViewModel.Model = fooModel;
+
+			Assert.IsTrue(propertyChangedSet.WaitOne(timeout));
+			Assert.AreEqual(fooModel,fooViewModel.Model);
+
+			fooViewModel.Model = null;
+
+			Assert.IsTrue(propertyChangedSet.WaitOne(timeout));
+			Assert.AreEqual(null, fooViewModel.Model);
+		}
+
+		private void FooViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			Assert.AreEqual("Model", e.PropertyName);
+			propertyChangedSet.Set();
+		}
+	}
+}
